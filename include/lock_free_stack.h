8da22301f970d088ea72bb2a95d90e3b6101f54c
Kunal Dubey <xakep8@protonmail.com>
2026-02-22 10:42:52 +0530

    refactor: update lock-free stack implementation and add tests

--- include/lock_free_stack.h
+++ include/lock_free_stack.h
@@ -1,6 +1,45 @@
 #pragma once
 
+#include <algorithm>
 #include <atomic>
+#include <mutex>
+#include <vector>
+
+constexpr uint64_t EPOCH_INACTIVE = UINT64_MAX;
+inline std::atomic<uint64_t> global_epoch{0};
+
+struct ThreadRecord {
+    std::atomic<uint64_t> active_epoch{EPOCH_INACTIVE};
+};
+
+inline std::vector<ThreadRecord*> thread_registry;
+inline std::mutex registry_mutex;
+
+inline thread_local ThreadRecord* local_record = [] {
+    auto* rec = new ThreadRecord();
+
+    std::lock_guard<std::mutex> lock(registry_mutex);
+    thread_registry.push_back(rec);
+
+    return rec;
+}();
+
+struct EpochGuard {
+    EpochGuard() {
+        uint64_t epoch = global_epoch.load(std::memory_order_acquire);
+        local_record->active_epoch.store(epoch, std::memory_order_release);
+    }
+
+    ~EpochGuard() { local_record->active_epoch.store(EPOCH_INACTIVE, std::memory_order_release); }
+};
+
+template <typename Node>
+struct RetiredNode {
+    Node* node;
+    uint64_t retire_epoch;
+};
+
+inline thread_local uint64_t local_epoch = EPOCH_INACTIVE;
 
 template <typename T>
 class LockFreeStack {
@@ -12,11 +51,89 @@ class LockFreeStack {
         Node(const T& value) : data(value), next(nullptr) {}
     } Node;
 
-    std::atomic<Node*> head;
+    Node* head;
+    std::mutex stack_mutex;
 
    public:
-    LockFreeStack() : head(nullptr) {};
+    static inline thread_local std::vector<RetiredNode<Node>> retire_list;
+
+   public:
+    LockFreeStack();
     ~LockFreeStack();
     void push(const T& value);
     bool pop(T& result);
+    void retire_node(Node* node);
+    void try_reclaim();
 };
+
+// Template implementations
+template <typename T>
+LockFreeStack<T>::LockFreeStack() {
+    head = nullptr;
+}
+
+template <typename T>
+LockFreeStack<T>::~LockFreeStack() {
+    std::lock_guard<std::mutex> lock(stack_mutex);
+    Node* node = head;
+
+    while (node) {
+        Node* next = node->next;
+        delete node;
+        node = next;
+    }
+
+    for (const auto& retired : retire_list) {
+        delete retired.node;
+    }
+    retire_list.clear();
+}
+
+template <typename T>
+void LockFreeStack<T>::push(const T& value) {
+    std::lock_guard<std::mutex> lock(stack_mutex);
+    Node* new_node = new Node(value);
+    new_node->next = head;
+    head = new_node;
+}
+
+template <typename T>
+bool LockFreeStack<T>::pop(T& result) {
+    std::lock_guard<std::mutex> lock(stack_mutex);
+    if (!head) return false;
+
+    Node* node = head;
+    head = node->next;
+    result = node->data;
+    delete node;
+    return true;
+}
+
+template <typename T>
+void LockFreeStack<T>::try_reclaim() {
+    uint64_t min_epoch = global_epoch.load(std::memory_order_acquire);
+
+    {
+        std::lock_guard<std::mutex> lock(registry_mutex);
+
+        for (ThreadRecord* rec : thread_registry) {
+            uint64_t active = rec->active_epoch.load(std::memory_order_acquire);
+            if (active != EPOCH_INACTIVE) min_epoch = std::min(min_epoch, active);
+        }
+    }
+
+    auto it = retire_list.begin();
+    while (it != retire_list.end()) {
+        if (it->retire_epoch + 2 <= min_epoch) {
+            delete it->node;
+            it = retire_list.erase(it);
+        } else {
+            ++it;
+        }
+    }
+}
+
+template <typename T>
+void LockFreeStack<T>::retire_node(Node* node) {
+    delete node;
+}
--- tests/lock_free_stack_test.cpp
+++ tests/lock_free_stack_test.cpp
@@ -1,7 +1,59 @@
-#include <gtest/gtest.h>
-
 #include "lock_free_stack.h"
 
-// TEST(LockFreeStack,){
+#include <gtest/gtest.h>
 
-// }
+#include <atomic>
+#include <thread>
+#include <vector>
+
+TEST(LockFreeStackTests, PushPopSingleThread) {
+    LockFreeStack<int> stack;
+
+    stack.push(10);
+    stack.push(20);
+
+    int value;
+    EXPECT_TRUE(stack.pop(value));
+    EXPECT_EQ(value, 20);
+
+    EXPECT_TRUE(stack.pop(value));
+    EXPECT_EQ(value, 10);
+
+    EXPECT_FALSE(stack.pop(value));
+}
+
+TEST(LockFreeStackTests, ConcurrentPushPop) {
+    LockFreeStack<int> stack;
+
+    constexpr int THREADS = 4;
+    constexpr int OPS_PER_THREAD = 100000;
+
+    std::atomic<int> push_count{0};
+    std::atomic<int> pop_count{0};
+
+    auto worker = [&]() {
+        for (int i = 0; i < OPS_PER_THREAD; ++i) {
+            stack.push(i);
+            push_count++;
+
+            int value;
+            if (stack.pop(value)) {
+                pop_count++;
+            }
+        }
+    };
+
+    std::vector<std::thread> threads;
+
+    for (int i = 0; i < THREADS; ++i) threads.emplace_back(worker);
+
+    for (auto& t : threads) t.join();
+
+    // Stack should be empty or near empty depending on interleaving
+    int remaining = 0;
+    int value;
+
+    while (stack.pop(value)) remaining++;
+
+    EXPECT_EQ(push_count.load(), pop_count.load() + remaining);
+}
