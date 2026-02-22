efe0e73bd8e0add383af3692794d8427b5c51384
Kunal Dubey <xakep8@protonmail.com>
2026-02-22 11:00:00 +0530

    refactor: simplify lock-free stack implementation and remove unnecessary mutexes

--- include/lock_free_stack.h
+++ include/lock_free_stack.h
@@ -1,45 +1,10 @@
 #pragma once
 
-#include <algorithm>
 #include <atomic>
-#include <mutex>
 #include <vector>
 
-constexpr uint64_t EPOCH_INACTIVE = UINT64_MAX;
 inline std::atomic<uint64_t> global_epoch{0};
-
-struct ThreadRecord {
-    std::atomic<uint64_t> active_epoch{EPOCH_INACTIVE};
-};
-
-inline std::vector<ThreadRecord*> thread_registry;
-inline std::mutex registry_mutex;
-
-inline thread_local ThreadRecord* local_record = [] {
-    auto* rec = new ThreadRecord();
-
-    std::lock_guard<std::mutex> lock(registry_mutex);
-    thread_registry.push_back(rec);
-
-    return rec;
-}();
-
-struct EpochGuard {
-    EpochGuard() {
-        uint64_t epoch = global_epoch.load(std::memory_order_acquire);
-        local_record->active_epoch.store(epoch, std::memory_order_release);
-    }
-
-    ~EpochGuard() { local_record->active_epoch.store(EPOCH_INACTIVE, std::memory_order_release); }
-};
-
-template <typename Node>
-struct RetiredNode {
-    Node* node;
-    uint64_t retire_epoch;
-};
-
-inline thread_local uint64_t local_epoch = EPOCH_INACTIVE;
+inline thread_local uint64_t local_epoch = 0;
 
 template <typename T>
 class LockFreeStack {
@@ -47,35 +12,102 @@ class LockFreeStack {
     typedef struct Node {
         T data;
         Node* next;
+        std::atomic<bool> marked{false};
 
         Node(const T& value) : data(value), next(nullptr) {}
     } Node;
 
-    Node* head;
-    std::mutex stack_mutex;
-
-   public:
-    static inline thread_local std::vector<RetiredNode<Node>> retire_list;
+    std::atomic<Node*> head;
+    std::atomic<Node*> retired_head;
 
    public:
     LockFreeStack();
     ~LockFreeStack();
     void push(const T& value);
     bool pop(T& result);
-    void retire_node(Node* node);
-    void try_reclaim();
+    void retire_node(Node* node, uint64_t retire_epoch);
+    void try_advance_epoch();
+    void graceful_cleanup();
 };
 
 // Template implementations
 template <typename T>
 LockFreeStack<T>::LockFreeStack() {
-    head = nullptr;
+    head.store(nullptr, std::memory_order_relaxed);
+    retired_head.store(nullptr, std::memory_order_relaxed);
+    local_epoch = global_epoch.load(std::memory_order_acquire);
 }
 
 template <typename T>
 LockFreeStack<T>::~LockFreeStack() {
-    std::lock_guard<std::mutex> lock(stack_mutex);
-    Node* node = head;
+    graceful_cleanup();
+}
+
+template <typename T>
+void LockFreeStack<T>::push(const T& value) {
+    uint64_t epoch = global_epoch.load(std::memory_order_acquire);
+    local_epoch = epoch;
+
+    Node* new_node = new Node(value);
+
+    Node* old_head = head.load(std::memory_order_relaxed);
+    do {
+        new_node->next = old_head;
+    } while (!head.compare_exchange_weak(old_head, new_node, std::memory_order_release, std::memory_order_relaxed));
+}
+
+template <typename T>
+bool LockFreeStack<T>::pop(T& result) {
+    uint64_t epoch = global_epoch.load(std::memory_order_acquire);
+    local_epoch = epoch;
+
+    while (true) {
+        Node* old_head = head.load(std::memory_order_acquire);
+
+        if (!old_head) {
+            return false;
+        }
+
+        if (old_head->marked.load(std::memory_order_acquire)) {
+            continue;
+        }
+
+        Node* new_head = old_head->next;
+
+        if (head.compare_exchange_weak(old_head, new_head, std::memory_order_acq_rel, std::memory_order_acquire)) {
+            result = old_head->data;
+            retire_node(old_head, epoch);
+            return true;
+        }
+    }
+}
+
+struct RetiredNode {
+    void* node;
+    uint64_t retire_epoch;
+};
+
+inline thread_local std::vector<RetiredNode> local_retired_list;
+
+template <typename T>
+void LockFreeStack<T>::retire_node(Node* node, uint64_t retire_epoch) {
+    node->marked.store(true, std::memory_order_release);
+
+    Node* old_retired = retired_head.load(std::memory_order_relaxed);
+    do {
+        node->next = old_retired;
+    } while (
+        !retired_head.compare_exchange_weak(old_retired, node, std::memory_order_release, std::memory_order_relaxed));
+}
+
+template <typename T>
+void LockFreeStack<T>::try_advance_epoch() {
+    // No-op: all cleanup deferred to destructor
+}
+
+template <typename T>
+void LockFreeStack<T>::graceful_cleanup() {
+    Node* node = head.exchange(nullptr, std::memory_order_acq_rel);
 
     while (node) {
         Node* next = node->next;
@@ -83,57 +115,16 @@ LockFreeStack<T>::~LockFreeStack() {
         node = next;
     }
 
-    for (const auto& retired : retire_list) {
-        delete retired.node;
-    }
-    retire_list.clear();
-}
+    node = retired_head.exchange(nullptr, std::memory_order_acq_rel);
 
-template <typename T>
-void LockFreeStack<T>::push(const T& value) {
-    std::lock_guard<std::mutex> lock(stack_mutex);
-    Node* new_node = new Node(value);
-    new_node->next = head;
-    head = new_node;
-}
-
-template <typename T>
-bool LockFreeStack<T>::pop(T& result) {
-    std::lock_guard<std::mutex> lock(stack_mutex);
-    if (!head) return false;
-
-    Node* node = head;
-    head = node->next;
-    result = node->data;
-    delete node;
-    return true;
-}
-
-template <typename T>
-void LockFreeStack<T>::try_reclaim() {
-    uint64_t min_epoch = global_epoch.load(std::memory_order_acquire);
-
-    {
-        std::lock_guard<std::mutex> lock(registry_mutex);
-
-        for (ThreadRecord* rec : thread_registry) {
-            uint64_t active = rec->active_epoch.load(std::memory_order_acquire);
-            if (active != EPOCH_INACTIVE) min_epoch = std::min(min_epoch, active);
-        }
+    while (node) {
+        Node* next = node->next;
+        delete node;
+        node = next;
     }
 
-    auto it = retire_list.begin();
-    while (it != retire_list.end()) {
-        if (it->retire_epoch + 2 <= min_epoch) {
-            delete it->node;
-            it = retire_list.erase(it);
-        } else {
-            ++it;
-        }
+    for (const auto& rn : local_retired_list) {
+        delete reinterpret_cast<Node*>(rn.node);
     }
-}
-
-template <typename T>
-void LockFreeStack<T>::retire_node(Node* node) {
-    delete node;
+    local_retired_list.clear();
 }
